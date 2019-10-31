2d782f2e23f5cd13aebbb4309fce84d5aa336294
wangyunlong <yunlong.wang@okcoin.com>
2019-10-31 08:25:10 +0800

     修改发布命令

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,8 +23,8 @@ module.exports = {
       host : '106.13.110.53',
       ref  : 'origin/master',
       repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      path : '/root/www',
+      'post-deploy' : 'git pull && npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
